c2f4a2a408c92405a13d725651d2661e98a09bc4
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-11 21:48:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ var funkyFunction = function () {
   }
   return "function";
 }
-var theFunk = funkyFunction;
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
